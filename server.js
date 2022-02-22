b94d854ed21e19239c73920cc63169ca27d39b9a
Emi <emi_chang@lhwlfw.com>
2022-02-22 15:32:55 +0800

    新增首頁與 404 頁面

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const http = require('http');
 
-const requestListener = (req, res) => {
-    console.log("req", req.method)
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello!");
+const request = (status, content) => {
+    res.writeHead(status, { "Content-Type": "text/html" });
+    res.write(content);
     res.end();
+}
+
+const requestListener = (req, res) => {
+    if (req.url === "/" && req.method === "GET") {
+        request(200, "<h1>index</h1>")
+    } else if (req.url === "/" && req.method === "DELETE") {
+        request(200, "<h1>刪除成功</h1>")
+    } else {
+        request(404, "Not Found 404.")
+    }
 }
 
 const server = http.createServer(requestListener);
